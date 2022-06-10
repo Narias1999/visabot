4366af1364dfc01c91b7a6ca84e7b47d70755223
Nicolas <nicolasarias870@gmail.com>
2022-06-09 20:39:42 -0500

    Add login feature

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
+const login = require('./src/login');
+
+const credentials = {
+  email: process.env.email,
+  password: process.env.password,
+};
 
 const startProcess = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'prod' });
   const page = await browser.newPage();
   await page.goto('https://ais.usvisa-info.com/en-co/niv/users/sign_in');
-  await page.screenshot({ path: 'example.png' });
-  await browser.close();
+  await login(page, credentials);
 }
 
 startProcess();
---
+++ src/login.js
@@ -0,0 +1,12 @@
+const login = async (page, credentials) => {
+  await page.waitForSelector('input.email');
+  await page.focus('input.email');
+  await page.keyboard.type(credentials.email);
+  await page.focus('input.password');
+  await page.keyboard.type(credentials.password);
+
+  await page.click('input#policy_confirmed');
+  await page.click('input[type=submit]');
+};
+
+module.exports = login;
